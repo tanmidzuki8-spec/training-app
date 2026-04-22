582311ffc339e99ea24ec139b0dea425ce48f995
tanmidzuki8-spec <tanmidzuki8@gmail.com>
2026-04-22 03:51:28 +0300

    Build updates and config for deployment

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"2lkulMo2q4nOefwj8t0CJyog1MW+gdHWJ5pDKg0sz88=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"FhAjZa9qursQTpVgdFTKVJRvOjK4AsJmMBXyRTUJSQ8=\"}"
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // если были другие настройки, оставь их:
+  // ...твои_другие_поля
+}
+
+export default nextConfig
